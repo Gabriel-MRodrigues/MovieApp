0179e3c4be99556fcda3e1700303b9a2f536992e
Gabriel-MRodrigues <gabrielmendesrodriguest@hotmail.com>
2025-04-22 16:49:40 -0300

    adding initial assets

--- MovieApp-React/src/App.jsx
+++ MovieApp-React/src/App.jsx
@@ -1,8 +1,19 @@
 function App() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
   );
 }
 
